230fde99bc18d7dbb99c88632c73622f02a3467e
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-03-31 20:13:02 +0200

    feat(app/server): add KAFKA_TOPIC to env

--- app/server/env.js
+++ app/server/env.js
@@ -7,8 +7,10 @@ const INFLUX_ORG = process.env.INFLUX_ORG || 'my-org'
 /** InfluxDB bucket  */
 const INFLUX_BUCKET = 'iot_center'
 
-/** KAFKA URL - optional URL to Kafka */
-const KAFKA_HOST = process.env.KAFKA_HOST || ''
+/** KAFKA URL - optional Kafka Host */
+const KAFKA_HOST = process.env.KAFKA_HOST
+/** KAFKA URL - optional Kafka Topic */
+const KAFKA_TOPIC = process.env.KAFKA_TOPIC
 
 // Defaults when on boarding a fresh new InfluxDB instance
 /** InfluxDB user  */
@@ -24,16 +26,23 @@ function logEnvironment() {
   console.log(`INFLUX_TOKEN=${INFLUX_TOKEN ? '***' : ''}`)
   console.log(`INFLUX_ORG=${INFLUX_ORG}`)
   console.log(`INFLUX_BUCKET=${INFLUX_BUCKET}`)
+  if (KAFKA_HOST) {
+    console.log(`KAFKA_HOST=${KAFKA_HOST}`)
+  }
+  if (KAFKA_TOPIC) {
+    console.log(`KAFKA_HOST=${KAFKA_HOST}`)
+  }
 }
 
 module.exports = {
   INFLUX_URL,
   INFLUX_TOKEN,
   INFLUX_ORG,
-  KAFKA_HOST,
   onboarding_username,
   onboarding_password,
   configuration_refresh,
   INFLUX_BUCKET,
   logEnvironment,
+  KAFKA_HOST,
+  KAFKA_TOPIC,
 }
