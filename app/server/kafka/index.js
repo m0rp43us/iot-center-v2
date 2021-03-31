351d8770aeb83834161e5079e99d4c7755f875be
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-03-31 22:50:11 +0200

    feat(app/server): use kafkajs to post data

--- app/server/kafka/index.js
+++ app/server/kafka/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const {KAFKA_HOST, KAFKA_TOPIC} = require('../env')
-const {KafkaClient, Producer} = require('kafka-node')
+const {Kafka} = require('kafkajs')
 
 // create kafka producer
-let kafkaClient
+let kafka
 if (KAFKA_HOST && KAFKA_TOPIC) {
-  console.log('Initializing Kafka producer')
-  kafkaClient = new KafkaClient({
-    kafkaHost: KAFKA_HOST,
-    connectTimeout: 5000,
+  console.log('Initializing Kafka client')
+  kafka = new Kafka({
+    clientId: 'iot-center_' + require('os').hostname(),
+    brokers: KAFKA_HOST.split(','),
   })
 }
 
@@ -17,40 +17,30 @@ const router = express.Router()
 router.use(express.text({limit: '10mb'}))
 // write endpoint that writes data to Kafka
 router.post('/api/v2/write', async (req, res) => {
-  if (!kafkaClient) {
+  if (!kafka) {
     res.status(500)
     res.end('Kafka is not configured!')
     return
   }
   const influxLineProtocolData = req.body
-  const producer = new Producer(kafkaClient)
   try {
-    await new Promise((resolve, reject) => {
-      producer.on('ready', () => {
-        producer.send(
-          {
-            topic: KAFKA_TOPIC,
-            messages: influxLineProtocolData,
-            attributes: 2 /* snappy compression */,
-          },
-          (err) => {
-            if (err) {
-              console.error('Kafka send fails:', err)
-              reject(err)
-            } else {
-              resolve()
-            }
-          }
-        )
-      })
-      producer.on('error', (e) => {
-        reject(e)
-      })
+    const producer = kafka.producer()
+    console.log('connecting')
+    await producer.connect()
+    console.log('sending')
+    await producer.send({
+      key: 'mykey',
+      topic: KAFKA_TOPIC,
+      messages: [{value: influxLineProtocolData}],
     })
+    console.log('message sent')
   } catch (e) {
     res.status(500)
     res.end('Kafka producer error: ' + e)
+    return
   }
+  res.status(204)
+  res.end()
 })
 
 module.exports = router
